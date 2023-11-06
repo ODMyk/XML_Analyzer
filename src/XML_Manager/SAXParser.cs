01089aca7956b52157a56ddb55ff3ca54022dec6
ODMyk <ostapenko.dmytro05@gmail.com>
2023-11-06 20:42:42 +0200

    Refactored code

--- src/XML_Manager/SAXParser.cs
+++ src/XML_Manager/SAXParser.cs
@@ -23,12 +23,20 @@ public class SAXParser : IParser
             var reader = XmlReader.Create(inputStream, XMLValidator.Settings);
             while (reader.Read())
             {
-                if (!(reader.NodeType == XmlNodeType.Element) || !(reader.Name == "Book")) continue;
+                if (!(reader.NodeType == XmlNodeType.Element && reader.Name == "Book")) continue;
                 var book = new Book();
-                for (int i = 0; i < 5; ++i)
-                {
-                    if (!ParseBookElement(reader, book)) return false;
-                }
+                SkipToText(reader);
+                book.Title = reader.Value;
+                SkipToText(reader);
+                book.Description = reader.Value;
+                SkipToText(reader);
+                book.Year = reader.Value;
+                SkipToText(reader);
+                book.Genre = reader.Value;
+                SkipToText(reader);
+                book.Author.FirstName = reader.Value;
+                SkipToText(reader);
+                book.Author.LastName = reader.Value;
 
                 Books.Add(book);
             }
@@ -40,45 +48,8 @@ public class SAXParser : IParser
         }
     }
 
-    private bool ParseBookElement(XmlReader reader, Book book) {
-        do { if (!reader.Read()) return false; } while (reader.NodeType != XmlNodeType.Element);
-        var name = reader.Name;
-        var conditionToSkipWhile = name != "Author" ? reader.NodeType != XmlNodeType.Text : reader.NodeType != XmlNodeType.Element;
-        do { if (!reader.Read()) return false; } while (conditionToSkipWhile);
-        switch (name)
-        {
-            case "Title":
-                book.Title = reader.Value;
-                break;
-
-            case "Description":
-                book.Description = reader.Value;
-                break;
-
-            case "Year":
-                book.Year = reader.Value;
-                break;
-
-            case "Genre":
-                book.Genre = reader.Value;
-                break;
-
-            case "Author":
-                if (reader.Name == "FirstName")
-                {
-                    do { if (!reader.Read()) return false; } while (reader.NodeType != XmlNodeType.Text);
-                    book.Author.FirstName = reader.Value;
-                    do { if (!reader.Read()) return false; } while (reader.NodeType != XmlNodeType.Text);
-                    book.Author.LastName = reader.Value;
-                    break;
-                }
-                do { if (!reader.Read()) return false; } while (reader.NodeType != XmlNodeType.Text);
-                book.Author.LastName = reader.Value;
-                do { if (!reader.Read()) return false; } while (reader.NodeType != XmlNodeType.Text);
-                book.Author.FirstName = reader.Value;
-                break;
-        }
-
-        return true;
+    private static void SkipToText(XmlReader reader)
+    {
+        do { if (!reader.Read()) throw new Exception(); } while (reader.NodeType != XmlNodeType.Text);
     }
 }
