b0f2dcbb42726f1d0819e65220577a321a47c8df
ODMyk <ostapenko.dmytro05@gmail.com>
2023-11-06 20:28:40 +0200

    Decomposition of large method

--- src/XML_Manager/SAXParser.cs
+++ src/XML_Manager/SAXParser.cs
@@ -27,43 +27,7 @@ public class SAXParser : IParser
                 var book = new Book();
                 for (int i = 0; i < 5; ++i)
                 {
-                    do { if (!reader.Read()) return false; } while (reader.NodeType != XmlNodeType.Element);
-                    var name = reader.Name;
-                    var conditionToSkipWhile = name != "Author" ? reader.NodeType != XmlNodeType.Text : reader.NodeType != XmlNodeType.Element;
-                    do { if (!reader.Read()) return false; } while (conditionToSkipWhile);
-                    switch (name)
-                    {
-                        case "Title":
-                            book.Title = reader.Value;
-                            break;
-
-                        case "Description":
-                            book.Description = reader.Value;
-                            break;
-
-                        case "Year":
-                            book.Year = reader.Value;
-                            break;
-
-                        case "Genre":
-                            book.Genre = reader.Value;
-                            break;
-
-                        case "Author":
-                            if (reader.Name == "FirstName")
-                            {
-                                do { if (!reader.Read()) return false; } while (reader.NodeType != XmlNodeType.Text);
-                                book.Author.FirstName = reader.Value;
-                                do { if (!reader.Read()) return false; } while (reader.NodeType != XmlNodeType.Text);
-                                book.Author.LastName = reader.Value;
-                                break;
-                            }
-                            do { if (!reader.Read()) return false; } while (reader.NodeType != XmlNodeType.Text);
-                            book.Author.LastName = reader.Value;
-                            do { if (!reader.Read()) return false; } while (reader.NodeType != XmlNodeType.Text);
-                            book.Author.FirstName = reader.Value;
-                            break;
-                    }
+                    if (!ParseBookElement(reader, book)) return false;
                 }
 
                 Books.Add(book);
@@ -74,5 +38,47 @@ public class SAXParser : IParser
         {
             return false;
         }
+    }
+
+    private bool ParseBookElement(XmlReader reader, Book book) {
+        do { if (!reader.Read()) return false; } while (reader.NodeType != XmlNodeType.Element);
+        var name = reader.Name;
+        var conditionToSkipWhile = name != "Author" ? reader.NodeType != XmlNodeType.Text : reader.NodeType != XmlNodeType.Element;
+        do { if (!reader.Read()) return false; } while (conditionToSkipWhile);
+        switch (name)
+        {
+            case "Title":
+                book.Title = reader.Value;
+                break;
+
+            case "Description":
+                book.Description = reader.Value;
+                break;
+
+            case "Year":
+                book.Year = reader.Value;
+                break;
+
+            case "Genre":
+                book.Genre = reader.Value;
+                break;
+
+            case "Author":
+                if (reader.Name == "FirstName")
+                {
+                    do { if (!reader.Read()) return false; } while (reader.NodeType != XmlNodeType.Text);
+                    book.Author.FirstName = reader.Value;
+                    do { if (!reader.Read()) return false; } while (reader.NodeType != XmlNodeType.Text);
+                    book.Author.LastName = reader.Value;
+                    break;
+                }
+                do { if (!reader.Read()) return false; } while (reader.NodeType != XmlNodeType.Text);
+                book.Author.LastName = reader.Value;
+                do { if (!reader.Read()) return false; } while (reader.NodeType != XmlNodeType.Text);
+                book.Author.FirstName = reader.Value;
+                break;
+        }
+
+        return true;
     }
 }
